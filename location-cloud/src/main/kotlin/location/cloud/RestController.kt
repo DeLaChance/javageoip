39ed468055ac476d359a36ea619e4a2666737f20
Lucien Valstar <lucien.valstar@asml.com>
2018-10-30 20:17:15 +0100

    Added unit test using spock

--- location-cloud/src/main/kotlin/location/cloud/RestController.kt
+++ location-cloud/src/main/kotlin/location/cloud/RestController.kt
@@ -1,9 +1,14 @@
 package location.cloud
 
-import io.micronaut.http.annotation.*
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
 
 @Controller("/")
 class HelloController {
+
+    init {
+        println("Started HelloController")
+    }
 
     @Get("/hello/{name}")
     fun hello(name: String): String {
