0034e9f9d55e84667b51d1f59baab9118a2a2ebe
Lucien Valstar <lucien.valstar@asml.com>
2018-11-19 15:00:35 +0100

    User can be added via post call

--- location-cloud/src/main/kotlin/nl/cloud/location/adapter/http/UserController.kt
+++ location-cloud/src/main/kotlin/nl/cloud/location/adapter/http/UserController.kt
@@ -1,13 +1,19 @@
 package nl.cloud.location.adapter.http
 
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
 import nl.cloud.location.domain.User
 
 @Controller("/api/users/")
 class UserController {
 
-    private val users: List<User> = listOf(User("John Snow"))
+    private val users: HashMap<String, User> = hashMapOf()
+
+    init {
+        this.addUser(User("John Snow"))
+    }
 
     @Get("/hello/{name}")
     fun hello(name: String): String {
@@ -16,6 +22,17 @@ class UserController {
 
     @Get("/")
     fun fetchUsers(): List<User> {
-        return users
+        return users.values.toList()
+    }
+
+    @Post("/")
+    fun createUser(@Body user: User): User {
+        this.addUser(user)
+
+        return user
+    }
+
+    private fun addUser(user: User) {
+        this.users.put(user.id, user)
     }
 }
--- location-cloud/src/test/groovy/UserControllerSpec.groovy
+++ location-cloud/src/test/groovy/UserControllerSpec.groovy
@@ -24,17 +24,30 @@ class UserControllerSpec extends Specification {
                 .retrieve(HttpRequest.GET('/api/users/hello/Lucien')) == "Hello Lucien"
     }
 
-    void "test that when the GET users api is invoked that the list of users is returned as a response"() {
+    void "test that when the fetch users api is invoked that the list of users is returned as a response"() {
         given:
         def objectMapper = new ObjectMapper()
 
         expect:
-        String response = client.toBlocking()
-            .retrieve(HttpRequest.GET('/api/users/'))
-
-        List<User> users = objectMapper.readValue(response, List.class)
+        List<User> users = client.toBlocking()
+            .retrieve(HttpRequest.GET('/api/users/'), List.class)
 
         users.size() == 1
         users[0].name == "John Snow"
+    }
+
+    void "test that when the create user api is invoked that the list of users contains that user"() {
+        given:
+        User user = new User("Eddard Stark")
+
+        when:
+        def returnedUser = client.toBlocking().retrieve(HttpRequest.POST("/api/users/", user), User.class)
+
+        then:
+        returnedUser.name == "Eddard Stark"
+
+        List<User> users = client.toBlocking().retrieve(HttpRequest.GET("/api/users"), List.class)
+        users.size() == 2
+        users[1].name == "Eddard Stark"
     }
 }
