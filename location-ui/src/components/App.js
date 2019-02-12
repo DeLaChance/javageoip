2bd4eb7c0f003c4ab0c049dbe2a771b737cc9b67
Lucien Valstar <lucien.valstar@asml.com>
2019-02-12 18:31:26 +0100

    Added titlebar and react material

--- location-ui/src/components/App.js
+++ location-ui/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 import RawTextView from "./RawTextView"
+import TitleBar from "./TitleBar";
 
 class App extends React.Component {
 
@@ -10,7 +11,10 @@ class App extends React.Component {
 
     render() {
         return (
-            <RawTextView />
+            <>
+                <TitleBar />
+                <RawTextView />
+            </>
         );
     }
 }
---
+++ location-ui/src/components/TitleBar.js
@@ -0,0 +1,30 @@
+import React from "react"
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+class TitleBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton color="inherit" aria-label="Menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" color="inherit">
+                        Location Tracker 
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        );
+    }
+}
+
+export default TitleBar;
